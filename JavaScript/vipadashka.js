39fb2a8b27c400d5d450a078158a17eb7b706881
ShokhinKh <107809872+ShokhinKh@users.noreply.github.com>
2024-12-21 00:30:10 +0500

    Add files via upload

---
+++ JavaScript/vipadashka.js
@@ -0,0 +1,6 @@
+const vsplivashka = document.querySelector('.vipadashka')
+const vsplivashkaBtn = document.querySelector('.menu-ava-img')
+
+vsplivashkaBtn.addEventListener("click", function(e){
+    vsplivashka.classList.toggle("active-vsplivashka")
+})
