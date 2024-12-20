6cb2a34d40817b6b19d8933ead81512df8005fef
ShokhinKh <107809872+ShokhinKh@users.noreply.github.com>
2024-12-21 01:37:41 +0500

    Update vipadashka.js

--- JavaScript/vipadashka.js
+++ JavaScript/vipadashka.js
@@ -4,3 +4,9 @@ const vsplivashkaBtn = document.querySelector('.menu-ava-img')
 vsplivashkaBtn.addEventListener("click", function(e){
     vsplivashka.classList.toggle("active-vsplivashka")
 })
+
+document.addEventListener('click', (event) => {
+    if (!vsplivashka.contains(event.target) && !vsplivashkaBtn.contains(event.target)) {
+        vsplivashka.classList.remove('active-vsplivashka');
+    }
+});
