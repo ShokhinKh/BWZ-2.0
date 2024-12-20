0e4e31de2c463cba3e56daad0d7efba71e5c7c8a
ShokhinKh <107809872+ShokhinKh@users.noreply.github.com>
2024-12-21 03:47:28 +0500

    Update vipadashka.js

--- JavaScript/vipadashka.js
+++ JavaScript/vipadashka.js
@@ -10,3 +10,21 @@ document.addEventListener('click', (event) => {
         vsplivashka.classList.remove('active-vsplivashka');
     }
 });
+
+
+document.getElementById("modal-edit-open").addEventListener("click", function(){
+    document.getElementById("form-modal-password").classList.add("open_modal")
+})
+
+document.getElementById("modal-password-close").addEventListener("click", function(){
+    document.getElementById("form-modal-password").classList.remove("open_modal")
+})
+
+document.querySelector("#form-modal-password .modal-item-password").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+})
+
+document.getElementById("form-modal-password").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove("open_modal")
+})
